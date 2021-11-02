935e63f6f98163357393ced5349594cfefebaf7b
bainiao <bainiao@163.com>
2021-11-02 18:09:54 +0800

    first commit

--- src/com/bainiao/git/GitTest.java
+++ src/com/bainiao/git/GitTest.java
@@ -3,5 +3,6 @@ package com.bainiao.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("woqu");
     }
 }
